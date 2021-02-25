ebd15ef16b8b14b41167afb372db34b5af041cbc
Martin Matak <martin.matak@gmail.com>
2021-02-24 17:01:03 -0700

    remove debug statements, add publishing pcd

--- scripts/demo.py
+++ scripts/demo.py
@@ -12,15 +12,35 @@ MESH_PATH = pkg_path + "/data/mesh.stl"
 CLOUD_PATH = pkg_path + "/data/cloud.pcd"
 
 def main():
+    rospy.init_node("demo")
     rospy.wait_for_service(SERVICE_NAME)
     req = rospy.ServiceProxy(SERVICE_NAME, MeshToCloudPose)
 
     mesh_path = MESH_PATH
-    object_cloud = PointCloud.from_path(CLOUD_PATH)
+
+    pc = PointCloud.from_path(CLOUD_PATH)
+    pc.pc_data = [1,2,3]
+    
+    object_cloud = pc
+    pcd_pub = rospy.Publisher("pcd", sensor_msgs.msg.PointCloud2, queue_size=1)
+    pcd_msg = object_cloud.to_msg()
+    pcd_msg.header.frame_id = "world"
+    pcd_msg.header.stamp = rospy.Time.now()
+    print(pcd_msg.header)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        pcd_pub.publish(pcd_msg)
+        rate.sleep()
+    print("published")
+
+
+    '''
     table_cloud = PointCloud.from_path(CLOUD_PATH)
 
     resp=req(mesh_path, object_cloud.to_msg(), table_cloud.to_msg())
     print(resp)
+    '''
+
 
 
 
